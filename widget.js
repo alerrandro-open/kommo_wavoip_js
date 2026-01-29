0e6a57d30398670858d447c9d5b8b9f5b8cde64d
alerrandro-open <alerrandro@openexo.com.br>
2026-01-29 18:28:30 -0300

    Update widget.js

--- widget.js
+++ widget.js
@@ -5,8 +5,20 @@ define(function () {
         console.log('[KOMMO] init OK');
         return true;
       },
+
       render: function () {
         console.log('[KOMMO] render OK');
+
+        if (this.widgets && this.widgets.add_action) {
+          this.widgets.add_action('card', {
+            text: 'BOTÃO TESTE',
+            class_name: 'kommo-test-btn',
+            onClick: function () {
+              alert('BOTÃO FUNCIONANDO 🎉');
+            }
+          });
+        }
+
         return true;
       }
     };

85b6ce1798e3629a4c0007eae76167df5d50774a
alerrandro-open <alerrandro@openexo.com.br>
2026-01-29 18:18:02 -0300

    Update widget.js

--- widget.js
+++ widget.js
@@ -2,22 +2,11 @@ define(function () {
   return function () {
     this.callbacks = {
       init: function () {
-        console.log('[KOMMO TEST] init OK');
+        console.log('[KOMMO] init OK');
         return true;
       },
-
       render: function () {
-        console.log('[KOMMO TEST] render OK');
-
-        if (this.widgets && this.widgets.add_action) {
-          this.widgets.add_action('card', {
-            text: 'TESTE BOTÃO',
-            onClick: function () {
-              alert('BOTÃO FUNCIONANDO');
-            }
-          });
-        }
-
+        console.log('[KOMMO] render OK');
         return true;
       }
     };
